0232500cb9d3cbb8e61e7e8d2797467c0d4cc20d
samcdonald14 <50050303@belfastmet.ac.uk>
2024-11-20 12:01:43 +0000

    Merge remote-tracking branch 'origin/main'

--- Task 1.py
+++ Task 1.py
@@ -14,13 +14,16 @@ cpu_health = float(input("Please enter your CPU usage percentage as an integer."
 #Then because i used an if previously i used an elif statement to dictate what the other outcome would be if the input did not meet the requirements of the if statement.
 #I have used and to show a range of numbers this is telling the program that between to floats is the desired input to get that particular answer.
 #I then use an else statement in order to dictate that anything outside of the if or elif will give a certain result
+# I used a while loop in order to allow the user to try and enter another input if they choose a number that isn't between 0-100.
+#The loop will continue until the user inputs a value that correlates with the rule that is dictated within the loop
+#Once the user inputs a correct value the loop will break and go on to the next stage of the program.
 while True:
     try:
         cpu_health = float(input("Please enter your CPU usage percentage as an integer (0-100): "))
         if cpu_health < 0 or cpu_health > 100:
             print("You have entered an invalid percentage. Please enter a value between 0 and 100.")
         else:
-            break  # Exit the loop if the input is valid
+            break
     except ValueError:
         print("Invalid input! Please enter a numeric value for CPU percentage.")
 if cpu_health < 40:
@@ -35,18 +38,25 @@ else:
 #Then because i used an if previously i used an elif statement to dictate what the other outcome would be if the input di not meet the requirements of the if statement.
 #I have used and to show a range of numbers this is telling the program that between to floats is the desired input to get that particular answer.
 #I then use an else statement in order to dictate that anything outside of the if or elif will give a certain result
-print ("Now we will check your memory usage (RAM)")
-memory_usage = float(input("Please enter your memory usage (GB) as an integer:"))
-if memory_usage < 0 or memory_usage > 64:
-    print (" you have entered an invalid value, could you please try again with a value between 0 and 64.")
-elif memory_usage < 4:
-        print("Your RAM is being underutilised.")
-elif memory_usage >= 4 and memory_usage <=8:
-    print("Your RAM is being optimally utilised.")
+print("Now we will check your memory usage (RAM)")
+
+while True:
+    try:
+        memory_usage = float(input("Please enter your memory usage (GB) as a number:"))
+        if memory_usage < 0 or memory_usage > 64:
+            print("You have entered an invalid value. Please enter a value between 0 and 64.")
+        else:
+            break
+    except ValueError:
+        print("You have entered an invalid value, could you please try again with a value between 0 and 64.")
+
+if memory_usage < 4:
+    print("Your RAM is being underutilized.")
+elif 4 <= memory_usage <= 8:
+    print("Your RAM is being optimally utilized.")
 else:
-    print("Your RAM is overloaded, please consider closing processes, tabs or programs on your computer.")
-#Here i print out the cosing message for the program using the print command
+    print("Your RAM is overloaded, please consider closing processes, tabs, or programs on your computer.")
+
+
 print("These are the results of your computer health check")
 print(f"Thank you for using the TechComp computer monitoring bot.")
-
-print ("test")
