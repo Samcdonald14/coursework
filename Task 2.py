4baaadc1147b85a1373ccef5a685e49863188756
samcdonald14 <50050303@belfastmet.ac.uk>
2024-11-20 16:31:13 +0000

    Merge remote-tracking branch 'origin/main'

--- Task 2.py
+++ Task 2.py
@@ -8,12 +8,24 @@ print ("Welcome to the Moodsense chatbot,\nThis application will advise you what
 #if the score is between 4-7 it will accept the answer and give a recommendation, the elif statement is used below the if, it is used when the if statement is not true.
 #I have used and to show a range of numbers this is telling the program that between to floats is the desired input to get that particular answer.
 #the else statement then congratulates the user on any other number they input because the two other statement already dictated the other scores between 1-10
-mood_score = float(input("Please enter your mood as a score from 1-10, we will then advise you what steps you should take!:"))
-if mood_score < 1 or mood_score > 10:
-    print("You have entered an invalid integer Please enter a value between 1 and 10.")
-elif mood_score<4:
-    print ("Sorry you don't feel good, you should consider speaking to someone whether a friend, loved one or therapist.")
+#I used a while loop in order to allow the user to try and enter another input if they choose a number that isn't between 0-100.
+#The loop will continue until the user inputs a value that correlates with the rule that is dictated within the loop.
+#Once the user inputs a correct value the loop will break and go on to the next stage of the program.
+#The value error is used in order to ensure that the user inputs an integer and not letters for example.
+#The value error exception is raised when the input cannot be interpreted as a float it is used in order to stop the user inputting strings or special characters.
+while True:
+    try:
+        mood_score = float(input("Please enter your mood as a score from 1-10, we will then advise you what steps you should take!:"))
+        if mood_score < 1 or mood_score > 10:
+            print("You have entered an invalid integer. Please enter a value between 1 and 10.")
+        else:
+            break
+    except ValueError:
+        print("Invalid input! Please enter a numeric value between 1 and 10.")
+
+if mood_score < 4:
+    print("Sorry you don't feel good, you should consider speaking to someone whether a friend, loved one, or therapist.")
 elif mood_score >= 4 and mood_score <= 7:
-    print ("Your mood is okay, try to remember to look after yourself both physically and mentally")
+    print("Your mood is okay, try to remember to look after yourself both physically and mentally.")
 else:
-    print ("Its great to hear that you are feeling so good!!")
+    print("It's great to hear that you are feeling so good!!")
--- while.py
+++ while.py
@@ -1,9 +1,10 @@
 
 while True:
     try:
-        memory_usage = float(input("Please enter your memory usage (GB) as an integer:"))
-        if memory_usage < 0 or memory_usage > 64:
-            print("You have entered an invalid percentage. Please enter a value between 0 and 100.")
+        mood_score = float(
+            input("Please enter your mood as a score from 1-10, we will then advise you what steps you should take!:"))
+        if mood_score < 1 or mood_score > 10:
+            print("You have entered an invalid integer Please enter a value between 1 and 10.")
         else:
             break  # Exit the loop if the input is valid
     except ValueError:
